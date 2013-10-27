f297b4f9adf8d9f70d9b828568cd7a54ef20f322
Christoph Neuroth <christoph.neuroth@gmail.com>
2013-10-27 18:31:07 +0100

    exercise 4.1

---
+++ Chapter4Exercises.hs
@@ -0,0 +1,26 @@
+import Chapter4
+import Test.QuickCheck
+
+-------------------------------------------------------------------------------
+-- Exercise 4.1
+-------------------------------------------------------------------------------
+
+-- Using maxThree, but not max
+maxFourA :: Int -> Int -> Int -> Int -> Int
+maxFourA a b c d
+    | (maxThree a b c) >= d   = maxThree a b c
+    | otherwise               = d
+
+-- Using max only
+maxFourB :: Int -> Int -> Int -> Int -> Int
+maxFourB a b c d = (max (max (max a b) c) d)
+
+-- Using maxThree and max
+maxFourC :: Int -> Int -> Int -> Int -> Int
+maxFourC a b c d = (maxThree a b c) `max` d
+
+-- All three should yield the same result
+prop_maxFour :: Int -> Int -> Int -> Int -> Bool
+prop_maxFour a b c d =
+    maxFourA a b c d == maxFourB a b c d &&
+    maxFourB a b c d == maxFourC a b c d
