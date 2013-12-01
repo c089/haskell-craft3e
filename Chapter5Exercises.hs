bad7f0a7647ee0b58a9c1ffcf815dfff4471753a
Christoph Neuroth <christoph.neuroth@gmail.com>
2013-12-01 13:45:02 +0100

    Exercise 5.18 / 5.19

--- Chapter5Exercises.hs
+++ Chapter5Exercises.hs
@@ -64,3 +64,48 @@ perimeter (Circle r) = pi*2*r
 data DataItems = ShopItem' ProductName Quantity
 type ProductName = String
 type Quantity = Integer
+
+
+-------------------------------------------------------------------------------
+-- Exercise 5.18
+-------------------------------------------------------------------------------
+
+doubleAll :: [Integer] -> [Integer]
+doubleAll l = [ 2*e | e <- l ]
+
+testDoubleAll = TestCase (assertEqual "" [2,10,20] (doubleAll [1,5,10]))
+
+-------------------------------------------------------------------------------
+-- Exercise 5.19
+-------------------------------------------------------------------------------
+
+capitalize :: String -> String
+capitalize s = [ capitalizeChar c | c <- s ]
+
+capitalizeChar :: Char -> Char
+capitalizeChar 'a' = 'A'
+capitalizeChar 'A' = 'A'
+capitalizeChar 'b' = 'B'
+capitalizeChar 'B' = 'B'
+capitalizeChar 'c' = 'C'
+capitalizeChar 'C' = 'C'
+capitalizeChar 'd' = 'D'
+capitalizeChar 'D' = 'D'
+capitalizeChar 'e' = 'E'
+capitalizeChar 'E' = 'E'
+capitalizeChar x  = x
+-- and so on, or use Data.Char.isUpper but I thought that might be cheating
+
+testCapitalize = TestList
+    [ TestCase (assertEqual "all lower" "A B C" (capitalize "a b c"))
+    , TestCase (assertEqual "all upper" "A B C" (capitalize "A B C"))
+    , TestCase (assertEqual "mixed" "ABCDE" (capitalize "aBcdE"))
+    ]
+
+capitalizeLetters :: String -> String
+capitalizeLetters s = [ capitalizeChar c | c <- s, isLetter c ]
+
+isLetter :: Char -> Bool
+isLetter x = capitalizeChar x `elem` "ABCDE"
+
+testCapitalizeLetters = TestCase (assertEqual "" "ABC" (capitalizeLetters "a b c"))
